6b2edd2d51d8fe631e5853a69935e7f3577cedd6
André Davis <cubicle.jockey@live.com>
2018-04-03 16:28:15 -0700

    Checkpoint

--- 10-range.go
+++ 10-range.go
@@ -4,3 +4,15 @@
 
 package main
 
+import "fmt"
+
+func main(){
+
+    numbers := []int{2, 3, 4}
+    var sum int
+    for index, number := range numbers {
+        fmt.Println("Index [", index, "] has value: ", number)
+        sum += number
+    }
+}
+
