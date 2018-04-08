9216fac4ee3e081ea5f6730aa8f5c009c0b4e71e
André Davis <CubicleJockey@users.noreply.github.com>
2018-04-08 13:04:23 -0700

    Checkpoint

--- 36-mutexes.go
+++ 36-mutexes.go
@@ -27,6 +27,11 @@ func main() {
     var readOps uint64
     var writeOps uint64
 
+    /*
+    This starts 200 goroutines to issue reads to the state-owning goroutine via the reads channel.
+    Each read requires constructing a readOp, sending it over the reads channel,
+    and the receiving the result over the provided resp channel.
+     */
     for read := 0; read < 200; read++ {
         go func(){
             total := 0
@@ -42,6 +47,7 @@ func main() {
         }()
     }
 
+    //We start 100 writes as well, using a similar approach.
     for write := 0; write < 100; write++ {
         go func(){
             for {
